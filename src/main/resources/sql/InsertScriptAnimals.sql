fa025d3a66a3ac644b3f5e67296cad5892cbc208
Bianca Burema <biancaburema@gmail.com>
2022-01-28 15:39:24 +0100

    deleted sql folder and insert script

--- src/main/resources/sql/InsertScriptAnimals.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM crittrDB.animal;
-
-INSERT INTO animal VALUES (1, '1990-03-25', 'Tim', 'Turtle'); 
-INSERT INTO animal VALUES (2, '2021-05-30', 'Geertje', 'Goat');
-INSERT INTO animal VALUES (3, '2020-08-20', 'Clara', 'Cow');
-INSERT INTO animal VALUES (4, '2018-06-01', 'Charlie', 'Chicken');
-INSERT INTO animal VALUES (5, '2015-10-4', 'Roger', 'Rabbit');
-INSERT INTO animal VALUES (6, '2018-07-04', 'Geeske', 'Guinea Pig');
