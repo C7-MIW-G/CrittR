1be3d71b405f7140f8776b9105a553d47b987e9a
Annemiek Blaauwgeers <annemiekblaauwgeers@gmail.com>
2022-02-14 15:32:26 +0100

    CR-94 Changed Animal Status display name

--- src/main/java/com/miw/databeestjes/crittr/model/AnimalStatus.java
+++ src/main/java/com/miw/databeestjes/crittr/model/AnimalStatus.java
@@ -1,8 +1,18 @@
 package com.miw.databeestjes.crittr.model;
 
 public enum AnimalStatus {
-    INCOMING,
-    PRESENT,
-    RELOCATED,
-    DECEASED;
+    INCOMING ("Incoming"),
+    PRESENT ("Present"),
+    RELOCATED ("Relocated"),
+    DECEASED ("Deceased");
+
+    private String displayName;
+
+    AnimalStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
 }
