c0b7205f38d79c4c0d55f6bd52265a53298c3d61
Milo A. Ottenhoff <m.a.ottenhoff@st.hanze.nl>
2022-02-16 09:33:41 +0100

    Created CrittrJS javascript file and used it to achieve report button dependency on status

---
+++ src/main/resources/static/js/crittrJS.js
@@ -0,0 +1,22 @@
+function setButtons(currentStatus) {
+    switch (currentStatus) {
+        case 0 : document.getElementById('accept-form').style.display = 'block';
+            document.getElementById('discard-form').style.display = 'block';
+            break;
+        case 1 : document.getElementById('observe-form').style.display = 'block';
+            document.getElementById('close-form').style.display = 'block';
+            break;
+        case 2 : setReopenButton();
+            break;
+        case 3 : document.getElementById('close-form').style.display = 'block';
+            setReopenButton();
+            break;
+        case 4 : setReopenButton();
+            break;
+    }
+}
+
+function setReopenButton() {
+    document.getElementById('accept-form').style.display = 'block';
+    document.getElementById('accept-button').value = 'Reopen';
+}
