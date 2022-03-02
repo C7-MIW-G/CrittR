1bf053f03f75f483d5eed5e8d0408a01a2993712
Milo A. Ottenhoff <m.a.ottenhoff@st.hanze.nl>
2022-03-02 10:15:40 +0100

    CR-87 Removed unused import statement in ColourController

--- src/main/java/com/miw/databeestjes/crittr/controller/ColourController.java
+++ src/main/java/com/miw/databeestjes/crittr/controller/ColourController.java
@@ -6,7 +6,6 @@ import com.miw.databeestjes.crittr.model.UserRoleStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
