629b01d6d88ca67fa49019122e5e0d56ac5aa2c2
Annemiek Blaauwgeers <annemiekblaauwgeers@gmail.com>
2022-03-04 09:55:30 +0100

    Removed unused imports

--- src/main/java/com/miw/databeestjes/crittr/dto/CrittrUserDTO.java
+++ src/main/java/com/miw/databeestjes/crittr/dto/CrittrUserDTO.java
@@ -1,24 +1,8 @@
 package com.miw.databeestjes.crittr.dto;
 
-import com.miw.databeestjes.crittr.model.Animal;
 import com.miw.databeestjes.crittr.model.CrittrUser;
-import com.miw.databeestjes.crittr.model.Report;
-import com.miw.databeestjes.crittr.model.UserRoleStatus;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
 
 /**
  * @author Milo Ottenhoff <m.a.ottenhoff@st.hanze.nl
