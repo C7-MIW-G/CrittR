8aa8fbafb7e5993ef0492acc3c8bea625875c6c0
Bianca Burema <biancaburema@gmail.com>
2022-03-08 11:34:28 +0100

    cr-107 changes some things to fun facts like miles and feet

--- src/main/java/com/miw/databeestjes/crittr/seeding/FunFactSeeder.java
+++ src/main/java/com/miw/databeestjes/crittr/seeding/FunFactSeeder.java
@@ -24,13 +24,14 @@ public class FunFactSeeder {
         if (funFactService.getAll().size() == 0) {
             funFactService.addFunFact("Cows can sleep standing up, but they can only dream lying down.");
             funFactService.addFunFact("Just like you, cows have best friends. Not only does companionship make cows happier, it also makes them smarter.");
+
             funFactService.addFunFact("Goats were one of the first animals to be tamed by humans and were being herded 9,000 years ago.");
-            funFactService.addFunFact("Goats can be taught their name and to come when called");
+            funFactService.addFunFact("Goats can be taught their name and to come when called.");
             funFactService.addFunFact("Goats’ pupils (like many hooved animals) are rectangular. This gives them vision for 320 to 340 degrees (compared to humans with 160-210) around them without having to move and they are thought to have excellent night vision.");
 
             funFactService.addFunFact("Pigs have the intelligence of a human toddler and are ranked as the fifth most intelligent animal in the world! In fact, pigs are more intelligent and trainable than any breed of dog.");
-            funFactService.addFunFact("Pigs are navigators: they can find their way home over large distances. They can often trot long distances and can reach up to 11 miles per hour running.");
-            funFactService.addFunFact("The average fully grown pig can weigh between 300 and 700 pounds. However the world record for the heaviest pig has not been beaten since 1933. Big Bill from Tennessee weighed in at 2,552 pounds. Now that is a big pig.");
+            funFactService.addFunFact("Pigs are navigators: they can find their way home over large distances. They can often trot long distances and can reach up to 17 km per hour running.");
+            funFactService.addFunFact("The average fully grown pig can weigh between 135 and 315 kilo. However the world record for the heaviest pig has not been beaten since 1933. Big Bill from Tennessee weighed in at 1157 kilo. Now that is a big pig.");
             funFactService.addFunFact("Pigs have amazing memories. They can remember things for years and years and can recognize and remember people and places, sources of food and even how to use objects.");
 
             funFactService.addFunFact("Sheep are herbivores, which means their diet does not include meat. They typically eat seeds, grass and plants.");
@@ -51,7 +52,7 @@ public class FunFactSeeder {
 
             funFactService.addFunFact("To express happiness, bunnies will sometimes jump around and flick their heads and feet. That adorable behavior is known within the rabbit community as a “binky.");
             funFactService.addFunFact("A bunny’s big ears aren’t just for listening! They also help regulate the rabbit’s body temperatures. The ears’ blood vessels swell when it’s hot out, and contract when it’s cold.");
-            funFactService.addFunFact("Rabbits can hop and BOY can they also jump! In fact, rabbits can jump to impressive heights and distances….a little over 3 feet high and a whopping 10 feet long!");
+            funFactService.addFunFact("Rabbits can hop and BOY can they also jump! In fact, rabbits can jump to impressive heights and distances….a little over 90 cm high and a whopping 3 m long!");
             funFactService.addFunFact("The Guinness World Record for the largest rabbit is held by a 55-pound British pet rabbit named Ralph. He eats $90 of food a week!");
 
             funFactService.addFunFact("A male deer is called a buck but some larger males are referred to as stags. A Female deer is called a doe or hind. A young deer is called a fawn. Bucks and does can be distinguished from each other by the presence of antlers.");
