c02e25ff17aabee2bb5d427e13770b7add9726ea
IkBenIvo <i.l.didden@st.hanze.nl>
2022-03-11 14:23:06 +0100

    code cleanup

--- src/main/java/com/miw/databeestjes/crittr/controller/FunFactController.java
+++ src/main/java/com/miw/databeestjes/crittr/controller/FunFactController.java
@@ -1,20 +1,11 @@
 package com.miw.databeestjes.crittr.controller;
 
-import com.miw.databeestjes.crittr.model.Animal;
-import com.miw.databeestjes.crittr.model.CrittrUser;
 import com.miw.databeestjes.crittr.model.FunFact;
-import com.miw.databeestjes.crittr.model.Report;
 import com.miw.databeestjes.crittr.service.FunFactService;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
 
 /**
  * @author Ivo Didden <i.l.didden@st.hanze.nl>
