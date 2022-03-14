58de1f59eb3eb1ebf4d9d23e56eae969bee27c46
IkBenIvo <i.l.didden@st.hanze.nl>
2022-03-14 11:46:15 +0100

    Modal JS cleanup

--- src/main/resources/static/js/modalJS.js
+++ src/main/resources/static/js/modalJS.js
@@ -1,19 +1,16 @@
 const modal = document.getElementById("addFactModal");
-const btn = document.getElementById("addFactButton");
-const closeButton = document.getElementsByClassName("close-modal-x")[0];
-const cancelButton = document.getElementsByClassName("modal-cancel-btn")[0];
 
-btn.onclick = function() {
+$("#addFactButton").click(function(){
     modal.style.display = "block";
-}
+})
 
-closeButton.onclick = function() {
+$(".close-modal-x").click(function() {
     modal.style.display = "none";
-}
+})
 
-cancelButton.onclick = function() {
+$(".modal-cancel-btn").click(function() {
     modal.style.display = "none";
-}
+})
 
 window.onclick = function(event) {
     if (event.target == modal) {
