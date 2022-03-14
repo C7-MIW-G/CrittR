98ced4d47988a0fe652753cf182e60fd5995a0db
Milo A. Ottenhoff <m.a.ottenhoff@st.hanze.nl>
2022-03-14 11:25:28 +0100

    CR-108 Reworked filter functionality to user less code and generally be better - also fixed a bug where it stopped working because of a renamed class

--- src/main/resources/static/js/animalJS.js
+++ src/main/resources/static/js/animalJS.js
@@ -1,26 +1,26 @@
 let chosenSpecies = "";
 let chosenStatus = null;
 
-$('.button-column li').click(function() {
-    $(this).parent().find('li.active').removeClass('active');
+$('#species-list li').click(function() {
+    $('#species-list').find('li.active').removeClass('active');
+    $(this).addClass('active')
+
+    chosenSpecies = $(this).find('a').html();
+    console.log(chosenSpecies)
+    searchAnimals(chosenStatus, chosenSpecies);
+})
+
+$('#status-list li').click(function() {
+    $('#status-list').find('li.active').removeClass('active');
     $(this).addClass('active');
 
-    let speciesContent = document.getElementById('species-list')
-        .getElementsByClassName('active');
-
-    if(speciesContent.length > 0){
-            chosenSpecies = speciesContent[0].getElementsByTagName('a')[0].innerHTML;
-    }
-
-    let statusContent = document.getElementById('status-list').getElementsByClassName('active');
-    if(statusContent.length > 0) {
-            chosenStatus = statusContent[0].getElementsByTagName('a')[0].getAttribute('value');
-        }
+    chosenStatus = $(this).find('a').attr('value');
+    console.log(chosenStatus);
     searchAnimals(chosenStatus, chosenSpecies);
 })
 
 $('#all-animals-button').click(function(){
-    $('.button-column li').removeClass('active');
+    $('.filter-column li').removeClass('active');
     chosenSpecies = "";
     chosenStatus = null;
     searchAnimals(chosenStatus, chosenSpecies);
