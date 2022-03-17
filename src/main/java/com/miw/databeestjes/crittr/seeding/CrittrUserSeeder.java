ce254904cbf75fda03bb044524674e19aa501197
IkBenIvo <i.l.didden@st.hanze.nl>
2022-03-17 16:40:43 +0100

    Updated the seeder for the demo

--- src/main/java/com/miw/databeestjes/crittr/seeding/CrittrUserSeeder.java
+++ src/main/java/com/miw/databeestjes/crittr/seeding/CrittrUserSeeder.java
@@ -19,19 +19,16 @@ public class CrittrUserSeeder {
 
     public void seedCrittrUser() {
         if (crittrUserDetailsService.findAll().isEmpty()) {
-            crittrUserDetailsService.addCrittrUser("h.smith@crittr.com", "Harry Smith", "TemporaryPassword123", UserRoleStatus.ROLE_ADMIN);
-            crittrUserDetailsService.addCrittrUser("c.baker@crittr.com", "Caroline Baker", "TemporaryPassword123", UserRoleStatus.ROLE_CARETAKER);
+            crittrUserDetailsService.addCrittrUser("h.smith@crittr.com", "Harry Smith", "test", UserRoleStatus.ROLE_ADMIN);
+            crittrUserDetailsService.addCrittrUser("c.baker@crittr.com", "Caroline Baker", "test", UserRoleStatus.ROLE_CARETAKER);
             crittrUserDetailsService.addCrittrUser("l.brown@crittr.com", "Laura Brown", "TemporaryPassword123", UserRoleStatus.ROLE_CARETAKER);
             crittrUserDetailsService.addCrittrUser("o.davies@crittr.com", "Oliver Davies", "TemporaryPassword123", UserRoleStatus.ROLE_CARETAKER);
             crittrUserDetailsService.addCrittrUser("t.murphy@crittr.com", "Tracy Murphy", "TemporaryPassword123", UserRoleStatus.ROLE_CARETAKER);
-            crittrUserDetailsService.addCrittrUser("e.jones@member.com", "Emily Jones", "TemporaryPassword123", UserRoleStatus.ROLE_MEMBER);
+            crittrUserDetailsService.addCrittrUser("e.jones@member.com", "Emily Jones", "test", UserRoleStatus.ROLE_MEMBER);
             crittrUserDetailsService.addCrittrUser("j.obrien@member.com", "James O'Brien", "TemporaryPassword123", UserRoleStatus.ROLE_MEMBER);
             crittrUserDetailsService.addCrittrUser("w.wilson@member.com", "William Wilson", "TemporaryPassword123", UserRoleStatus.ROLE_MEMBER);
             crittrUserDetailsService.addCrittrUser("j.roberts@member.com", "Jessica Roberts", "TemporaryPassword123", UserRoleStatus.ROLE_MEMBER);
             crittrUserDetailsService.addCrittrUser("l.evans@member.com", "Lily Evans", "TemporaryPassword123", UserRoleStatus.ROLE_MEMBER);
-            crittrUserDetailsService.addCrittrUser("c@c.c", "C", "c", UserRoleStatus.ROLE_CARETAKER);
-            crittrUserDetailsService.addCrittrUser("a@a.a", "A", "a", UserRoleStatus.ROLE_ADMIN);
-            crittrUserDetailsService.addCrittrUser("m@m.m", "M", "m", UserRoleStatus.ROLE_MEMBER);
         }
     }
 }
